b9ca42df90172104c475e75ba84bd6421bf6792c
Melissa.Kinsey <kinsey.melissa@gmail.com>
2020-11-21 09:29:47 -0600

    Add most recent JS changes as I narrow down errors during debugging.

--- script.js
+++ script.js
@@ -1,71 +1,83 @@
-const characterCountRange = document.getElementById("characterCountRange");
-const characterCountNumber = document.getElementById("characterCountNumber");
-const includeUppercaseElement = document.getElementById("includeUppercase");
-const includeLowercaseElement = document.getElementById("includeLowercase");
-const includeNumbers = document.getElementById("includeNumbers");
-const includeSpecialCharacters = document.getElementById("specialCharacters");
-const form = document.getElementById("passwordGeneratorForm");
-const passwordDisplay = document.getElementById("passwordDisplay");
-// Set up event listener to sync number selected in box with number selected on slider.
-characterCountNumber.addEventListener("input", syncCharacterCount);
-characterCountRange.addEventListener("input", syncCharacterCount);
-
-// Prevent form from performing the default behavior of submitting and refreshing page when form is clicked.
-form.addEventListener("submit", (event) => {
-	event.preventDefault();
-	const characterCount = characterCountNumber.value;
-	// Set up event listener for checkboxes.
-	const includeUppercase = includeUppercaseElement.checked;
-	const includeLowercase = includeLowercaseElement.checked;
-	const includeNumbers = includeNumbersElement.checked;
-	const includeSpecialCharacters = includeSpecialCharacters.checked;
-	// Set up password variables.
-	const password = generatePassword(
-		characterCount,
-		includeUppercase,
-		includeLowercase,
-		includeNumbers,
-		includeSpecialCharacters
-	);
-	passwordDisplay.innerText = password;
-});
+const characterCountRange = document.getElementById("characterCountRange")
+const characterCountNumber = document.getElementById("characterCountNumber")
+const includeUppercaseElement = document.getElementById("includeUppercase")
+const includeNumbersElement = document.getElementById("includeNumbers")
+const includeSpecialElement = document.getElementById("includeSpecial")
+const form = document.getElementById("passwordGeneratorForm")
+const passwordDisplayElement = document.getElementById("passwordDisplay")
 
 // Add ASCII character codes for constants.
-const UPPERCASE_CHAR_CODES = ascendingArray(65, 90);
-const LOWERCASE_CHAR_CODES = ascendingArray(97, 122);
-const NUMBER_CHAR_CODES = ascendingArray(48, 57);
-const SPECIAL_CHAR_CODES = ascendingArray(33, 47);
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SPECIAL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+    arrayFromLowToHigh(58, 64)
+    ).concat(
+        arrayFromLowToHigh(91, 96)
+        ).concat(
+            arrayFromLowToHigh(123, 126)
+            )
+             
+            // Set up event listener to sync number selected in box with number selected on slider.
+            characterCountNumber.addEventListener("input", syncCharacterCount)
+            characterCountRange.addEventListener("input", syncCharacterCount)
+            
+            // Prevent form from performing the default behavior of submitting and refreshing page when form is clicked.
+            form.addEventListener("submit", e => {
+                e.preventDefault()
+                console.log("Is this working?")
+                const characterCount = characterCountNumber.value
+                // Set up event listener for checkboxes.
+                const includeUppercase = includeUppercaseElement.checked
+                const includeNumbers = includeNumbersElement.checked
+                const includeSpecial = includeSpecialElement.checked
+                // Set up password variables.
+                
+                const password = generatePassword(characterCount, includeUppercase, includeNumbers, includeSpecial)
+                const passwordDisplay = password
+                console.log("The password is " + passwordDisplay)
+            })
+            
+            function generatePassword(characterCount, includeUppercase, includeNumbers, includeSpecial) {
+                let charCodes = LOWERCASE_CHAR_CODES
+                if (includeUppercase) charCodes = charCodes.concat (UPPERCASE_CHAR_CODES)
+                if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+                if (includeSpecial) charCodes.concat(SPECIAL_CHAR_CODES)
 
-// Call function using ASCII character codes.
-function generatePassword(
-	characterCount,
-	includeUppercase,
-	includeLowercase,
-	includeNumbers,
-	includeSpecialCharacters
-) {
-	let charCodes = LOWERCASE_CHAR_CODES;
-	if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES);
-	if (includeSpecialCharacters)
-		charCodes = charCodes.concat(SPECIAL_CHARACTER_CHAR_CODES);
-	if (includesNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES);
-
-	const passwordCharacters = [];
-	for (let i = 0; i < characterCount; i++) {
-		const characterCode =
-			charCodes[Math.floor(Math.random() * charCodes.length)];
-		passwordCharacters.push(String.fromCharCode(characterCode));
-	}
-	return passwordCharacters.join("");
-}
-// Set up for loop to generate number arrays automatically.
-function ascendingArray(low, high) {
-	const array = [];
-	for (let i = low; i <= high; i++) array.push(i);
-}
-
-function syncCharacterCount(event) {
-	const value = event.target.value;
-	characterCountNumber.value = value;
-	characterCountRange.value = value;
-}
+                
+                const passwordCharacters = []
+                for (let i = 0; i < characterCount; i++) {
+                    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+                    passwordCharacters.push(String.fromCharCode(characterCode))
+                }
+                return passwordCharacters.join("")
+            }
+            
+            function arrayFromLowToHigh(low, high) {
+                const array = []
+                for (let i = low; i <= high; i++) {
+                    array.push(i)
+                }
+                return array
+            }
+            
+            function syncCharacterCount(e) {
+                const value = e.target.value
+                characterCountNumber.value = value
+                characterCountRange.value = value
+            }
+            
+            passwordDisplayElement.inner = passwordDisplay
+            
+            
+            
+            
+            
+            
+            // Call function using ASCII character codes.
+            
+            
+            
+            
+            // Set up for loop to generate number arrays automatically.
+            
